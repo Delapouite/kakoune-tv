3c268cd8e495f879399768906e96af9d562e5b5b
Delapouite <delapouite@gmail.com>
2017-01-25 19:11:35 +0100

    feat: add 5 more asciicasts

--- asciicasts.js
+++ asciicasts.js
@@ -4,5 +4,10 @@ var asciicasts = [
 {id: '4d1a7a05b8cb3409320001b4', keys: '%s<lt><ret>m<a-K>\n<ret>d,q'},
 {id: '4d1ac1800a045132c0000011', keys: 'wa<space><esc>xa<ret><esc>f)..f;>.f;>.,q'},
 {id: '4d665abd7d73e02a55000009', keys: '~iList<lt><esc>mc><esc><a-b>yAnew<space>Array<c-r>"();<esc>%s,<ret>Lc<ret><esc><a-C><a-&><a-x>Z<a-K>\.<ret>ad<a-;>z);<home>c.add(<esc><a-x>dm<a-x><a-R>,q'},
+{id: '4d6f45b938c0aa691b000003', keys: 'cabcdef<home><esc>Q<home>dP<a-p>Q4q,q'},
+{id: '4ddbd92898957e0001000016', keys: '12X<a-s>d%<a-s>p,q'},
+{id: '4e31627b74ab580001000007', keys: 'm<a-b>*5Nd<a-f>fwcvoid<space><esc>b*<space>cdouble<space><esc>ndbyggxR<a-/>\$<ret>d2<a-n>d%s<ret>./s<ret>iMath.<esc>gjxd,q'},
+{id: '4ef209ef78702b0001000019', keys: '%s# <ret>O<ret><esc>,q'},
+{id: '4fbf8e303be58b0001000024', keys: 'xs:<ret><a-f>VZ/}<ret>N<a-z>i<ret><up><esc>1<a-space>i<space><space><esc><a-x><a-k>E<ret>.n6<a-space>2ld,q'},
 {id: '571808767dd9d30009000001', keys: '%s,<ret>&,q'},
 ]
